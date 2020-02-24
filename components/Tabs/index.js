d58efe74ccf8509e37a6b9c296742442f38e840b
Bradley Drummonds <47236494+bdrummo6@users.noreply.github.com>
2020-02-23 20:08:12 -0500

    Completed Step 2 of the project
    Completed step 2 by making a GET request for data, analyzing the data, creating a Tab component, and then iterating through the data to create tabs for all topics.

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,30 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+// Retrieve reference to the .topics element
+const topicsContainer = document.querySelector('.topics');
+
+axios.get(`https://lambda-times-backend.herokuapp.com/topics`)
+   .then((response) => {
+      // console.log(response.data);
+      // console.log(response.data.topics);
+      const topics = response.data.topics;
+      topics.forEach((topic) => {
+         topicsContainer.appendChild(createTab(topic));
+      })
+   })
+   .catch((err) => {
+      console.log(`Error: ${err}`);
+   });
+
+// Function for creating a Tab component that takes a topic as an argument
+function createTab(topic) {
+   // Creates a 'div' which will bet tab component returned
+   const tab = document.createElement('div');
+   tab.classList.add('tab');
+   tab.textContent = topic;
+
+   return tab; // returns tab component
+
+}
